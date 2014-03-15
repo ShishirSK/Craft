7b98a2895e74b8c143b8fc732eba8c325e7f2758
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-03-15 01:35:48 -0700

    Put method impl

--- Data Structures/Trees/src/tree/datastructure/BST.java
+++ Data Structures/Trees/src/tree/datastructure/BST.java
@@ -1,29 +1,69 @@
 package tree.datastructure;
 
+/*
+ * A binary search tree implementation 
+ * 
+ * @author Shishir Kinkar
+ * 
+ */
 public class BST<Key extends Comparable<Key>, Value> {
 
 	private Node root;
 
+	/*
+	 * Definition of a tree node
+	 */
 	private class Node {
 		private Key key;
 		private Value val;
 		private Node left, right;
 
-		public void Node(Key key, Value val) {
+		public Node(Key key, Value val) {
 			this.key = key;
 			this.val = val;
 		}
-
 	}
 
-	public void put(Key key, Value val) {
+	/*
+	 * Method to put nodes into a BST
+	 * 
+	 * @return void - does not return a value
+	 * 
+	 * @param Key - key of the node, Val - value of the node
+	 */
+	public void put(Key Key, Value Val) {
+		root = put(root, Key, Val);
+	}
 
+	/*
+	 * Overloaded method to put nodes into a BST
+	 * 
+	 * @return Node - returns a node object
+	 * 
+	 * @param currentNode - current node object, pKey - key of the node, pVal -
+	 * value of the node
+	 */
+	private Node put(Node currentNode, Key pKey, Value pVal) {
+
+		if (currentNode == null)
+			return new Node(pKey, pVal);
+		int compareNode = pKey.compareTo(currentNode.key);
+		if (compareNode < 0) {
+			currentNode.left = put(currentNode.left, pKey, pVal);
+		} else if (compareNode > 0) {
+			currentNode.right = put(currentNode.right, pKey, pVal);
+		} else {
+			currentNode.val = pVal;
+		}
+		return currentNode;
 	}
 
 	/*
 	 * Method to get the value of a key from the tree
 	 * 
 	 * @return value - if key is present, null - if key is not present
+	 * 
+	 * @param checkKey - key to check
 	 */
 	public Value get(Key checkKey) {
 
@@ -32,11 +72,9 @@ public class BST<Key extends Comparable<Key>, Value> {
 			int compareResult = checkKey.compareTo(x.key);
 			if (compareResult < 0) {
 				x = x.left;
-			}
-			if (compareResult > 0) {
+			} else if (compareResult > 0) {
 				x = x.right;
-			}
-			if (compareResult == 0) {
+			} else {
 				return x.val;
 			}
 		}
