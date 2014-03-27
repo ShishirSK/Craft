39242b7042ff45c6fa4d924f1e1a35dbc37b71e1
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-03-27 00:27:30 -0700

    String rotate check completed

--- 100Q/Arrays and Strings/src/com/string/rotate/substring/StringRotate.java
+++ 100Q/Arrays and Strings/src/com/string/rotate/substring/StringRotate.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 /*
  * Program to check if one string is a rotation of other using isSubstring method 
  * 
- * NOTE: isSubstring is assumed to be a method that checks if one string is a 
- * substring of other
  */
 
 public class StringRotate {
@@ -19,21 +17,42 @@ public class StringRotate {
 		BufferedReader inputStrings = new BufferedReader(new InputStreamReader(
 				System.in));
 		try {
+			System.out.print("Enter the first string: ");
 			s1 = inputStrings.readLine();
+			System.out.print("Enter the second string: ");
 			s2 = inputStrings.readLine();
 		} catch (Exception e) {
 			System.out.println("Error:" + e);
 		}
 
-		checkIfRotation(s1, s2);
+		boolean isRotation = checkIfRotation(s1, s2);
+		if (isRotation)
+			System.out.println("One string is rotation of other.");
+		else {
+			System.out.println("One string is NOT the rotation of other.");
+		}
 	}
 
 	/*
 	 * Method to check if one string is rotated version of the other one
 	 */
+	public static boolean checkIfRotation(String first, String second) {
 
-	public static boolean checkIfRotation(String first, String Second) {
+		int l1 = first.length();
+		if (l1 == second.length() && l1 > 0) {
+			String doubleString = first + first;
+			return isSubstring(doubleString, second);
+		}
+		return false;
+	}
 
+	/*
+	 * Method to check if one string is substring of the other
+	 */
+	private static boolean isSubstring(String longer, String shorter) {
+		if (longer.contains(shorter)) {
+			return true;
+		}
 		return false;
 	}
 }
