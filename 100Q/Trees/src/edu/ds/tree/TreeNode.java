db5675579e6187b4ca5932f6e4d1100e9087b428
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-04-24 23:55:54 -0700

    TreeNode for Tree problems

---
+++ 100Q/Trees/src/edu/ds/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package edu.ds.tree;
+
+
+public class TreeNode<Key extends Comparable<AnyType>, AnyType> {
+
+	public Key key;
+	public AnyType value;
+	public TreeNode left, right;
+
+	public TreeNode(Key key, AnyType val) {
+		this.key = key;
+		this.value = val;
+	}
+	
+}
