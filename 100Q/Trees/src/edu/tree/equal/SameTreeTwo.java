755aa46950ff6d7675c4b95f88cff6592018b836
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-04-28 23:42:59 -0700

    Check if two trees are same

---
+++ 100Q/Trees/src/edu/tree/equal/SameTreeTwo.java
@@ -0,0 +1,29 @@
+package edu.tree.equal;
+
+import edu.ds.tree.TreeNode;
+
+/**
+ * Given two binary trees, write a function to check if they are equal or not.
+ * 
+ * Two binary trees are considered equal if they are structurally identical and
+ * the nodes have the same value. #LeetCode
+ * 
+ * @author Shishir Kinkar
+ *
+ */
+public class SameTreeTwo {
+
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+
+		if (p == null && q == null)
+			return true;
+		else if (p == null || q == null)
+			return false;
+
+		if (!(p.val == q.val && isSameTree(p.left, q.left) && isSameTree(
+				p.right, q.right)))
+			return false;
+		else
+			return true;
+	}
+}
