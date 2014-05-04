35bbc22efa68c42724b4f10a24a395e4f64212c5
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-05-04 02:14:09 -0700

    Linked list node for problems

---
+++ 100Q/Linked Lists/src/edu/ds/linkedlist/ListNode.java
@@ -0,0 +1,18 @@
+package edu.ds.linkedlist;
+
+/**
+ * Linked list node for using in problems
+ * 
+ * @author Shishir Kinkar
+ * 
+ */
+public class ListNode {
+
+	public int val;
+	public ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
