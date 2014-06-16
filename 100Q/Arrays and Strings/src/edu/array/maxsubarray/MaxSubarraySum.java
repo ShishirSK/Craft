14328148b81b965e386f3fc62880665da5470a41
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-06-15 21:35:18 -0700

    Find the max sum of sub-array in a given array

--- 100Q/Arrays and Strings/src/edu/array/maxsubarray/MaxSubarraySum.java
+++ 100Q/Arrays and Strings/src/edu/array/maxsubarray/MaxSubarraySum.java
@@ -1,5 +1,14 @@
 package edu.array.maxsubarray;
 
+/**
+ * Find the contiguous subarray within an array (containing at least one number)
+ * which has the largest sum. For example, given the array
+ * [-2,1,-3,4,-1,2,1,-5,4], the contiguous subarray [4,-1,2,1] has the largest
+ * sum = 6. #LeetCode
+ * 
+ * @author Shishir Kinkar
+ * 
+ */
 public class MaxSubarraySum {
 	public int maxSubArray(int[] A) {
 
@@ -14,6 +23,7 @@ public class MaxSubarraySum {
 			}
 		}
 
+		// If all are negative, return max negative
 		if (max < 0) {
 			return max;
 		} else {
