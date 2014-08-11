82319a3109493c71a1c4a146db59e1e3b82840dd
Shishir Kinkar <shishir.kinkar@gmail.com>
2014-08-11 16:49:36 -0700

    Create all subsets of the given array - II

---
+++ 100Q/Arrays and Strings/src/edu/array/subsets/SubsetsII.java
@@ -0,0 +1,54 @@
+package edu.array.subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given a collection of integers that might contain duplicates, S, return all
+ * possible subsets.
+ * 
+ * Note: Elements in a subset must be in non-descending order. The solution set
+ * must not contain duplicate subsets. #LeetCode
+ * 
+ * @author Shishir Kinkar
+ * 
+ */
+public class SubsetsII {
+
+	public List<ArrayList<Integer>> subsetsWithDup(int[] S) {
+
+		List<ArrayList<Integer>> subsets = new ArrayList<ArrayList<Integer>>();
+
+		// Empty subset
+		subsets.add(new ArrayList<Integer>());
+
+		// Always return an empty subset - []
+		if (S.length == 0) {
+			return subsets;
+		}
+
+		// To meet the ascending order condition
+		Arrays.sort(S);
+
+		// For the current integer value
+		for (int i = 0; i < S.length; i++) {
+			// This variable in need to keep tab on the count of subsets before
+			// you start adding new subsets to the same arraylist
+			int subsetCount = subsets.size();
+			// Add it to all the subsets
+			for (int j = 0; j < subsetCount; j++) {
+				// Make a copy of the current subset
+				ArrayList<Integer> newSet = (ArrayList<Integer>) subsets.get(j).clone();
+				// Add current integer to it.
+				newSet.add(S[i]);
+				// Add it to the final arraylist
+				if (!subsets.contains(newSet)) {
+					subsets.add(newSet);
+				}
+			}
+		}
+		
+		return subsets;
+	}
+}
